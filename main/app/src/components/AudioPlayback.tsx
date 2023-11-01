2c8114c8cba11840d2bb83b588655cefc037c9ae
deden3791 <aeden3791@gmail.com>
2023-11-01 16:01:02 +0000

    L4PROJ-11 implementing real-time gain manipluation

--- main/app/src/components/AudioPlayback.tsx
+++ main/app/src/components/AudioPlayback.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "../styles/styles.css";
 
 const AudioCapturePlayback = () => {
@@ -6,27 +6,38 @@ const AudioCapturePlayback = () => {
   const [microphoneStream, setMicrophoneStream] = useState<MediaStream | null>(null);
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
   const [microphoneSourceNode, setMicrophoneSourceNode] = useState<MediaStreamAudioSourceNode | null>(null);
+  const [gainNode, setGainNode] = useState<GainNode | null>(null);
+  const [volume, setVolume] = useState(0.5); // Initial volume value
+
+  useEffect(() => {
+    if (audioContext) {
+      if (gainNode) {
+        gainNode.gain.value = volume; // Set the volume
+      }
+    }
+  }, [audioContext, gainNode, volume]);
+
+  const handleVolumeChange = (event: any) => {
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+  };
 
   const handlePlayAudio = async () => {
     try {
       if (!isAudioPlaying) {
-        const audioContext = new AudioContext();
-        if (audioContext === null) {
-          setAudioContext(new AudioContext());
-        }
+        const context = new AudioContext();
+        setAudioContext(context);
+        const gain = context.createGain();
+        setGainNode(gain);
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         setMicrophoneStream(stream);
-        setAudioContext(audioContext);
-
-        const sourceNode = audioContext.createMediaStreamSource(stream);
+        const sourceNode = context.createMediaStreamSource(stream);
         setMicrophoneSourceNode(sourceNode);
-
-        // Connect the microphone source to the destination
-        sourceNode.connect(audioContext.destination);
+        sourceNode.connect(gain);
+        gain.connect(context.destination);
       } else {
         if (microphoneSourceNode) {
           microphoneSourceNode.disconnect();
-          setMicrophoneSourceNode(null);
         }
         if (microphoneStream) {
           microphoneStream.getTracks().forEach((track) => {
@@ -37,6 +48,10 @@ const AudioCapturePlayback = () => {
         if (audioContext) {
           audioContext.close();
           setAudioContext(null);
+        }
+        if (gainNode) {
+          gainNode.disconnect();
+          setGainNode(null);
         }
       }
       setIsPlaying(!isAudioPlaying);
@@ -51,6 +66,20 @@ const AudioCapturePlayback = () => {
         <button className="small-button" onClick={handlePlayAudio}>
           {isAudioPlaying ? 'Stop Microphone' : 'Start Microphone'}
         </button>
+      </div>
+
+      <div className="controls">
+        <label className='small-text'>Volume: </label>
+        <input
+          type="range"
+          id="volume"
+          name="volume"
+          min="0"
+          max="1"
+          step="0.01"
+          value={volume}
+          onChange={handleVolumeChange}
+        />
       </div>
     </div>
   );
