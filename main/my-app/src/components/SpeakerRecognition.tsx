d11447af1420c4ddeaa504a3b57835e6fc4d185f
deden3791 <aeden3791@gmail.com>
2023-11-27 16:52:46 +0000

    L4PROJ-18 trying to implement eagle API

--- main/my-app/src/components/SpeakerRecognition.tsx
+++ main/my-app/src/components/SpeakerRecognition.tsx
@@ -18,42 +18,10 @@ const SpeakerRecognition = async () => {
   //         ${ACCESS_KEY},
   //         eagleModel);
 
-  async function getAudioData(numSamples: number) {
-    const audioContext = new (window.AudioContext || window.AudioContext)();
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const source = audioContext.createMediaStreamSource(stream);
-      const processor = audioContext.createScriptProcessor(4096, 1, 1);
-  
-      let chunks: Float32Array[] = [];
-      processor.onaudioprocess = (e) => {
-        const inputData = e.inputBuffer.getChannelData(0);
-        chunks.push(new Float32Array(inputData));
-      };
-  
-      source.connect(processor);
-      processor.connect(audioContext.destination);
-  
-      await new Promise<void>((resolve) => {
-        setTimeout(() => {
-          processor.disconnect();
-          source.disconnect();
-          stream.getTracks().forEach((track) => track.stop());
-          const audioData = chunks.flat().slice(0, numSamples);
-          const int16Data = audioData.map((value: any) => Math.round(value * 32767));
-          resolve();
-        }, 1000); // Adjust the duration as needed to capture the required audio
-      });
-  
-      const audioData = chunks.flat().slice(0, numSamples);
-      const int16Data = audioData.map((value: any) => Math.round(value * 32767));
-      return new Int16Array(int16Data);
-    } catch (error) {
-      // Handle errors (e.g., user denied microphone access)
-      throw new Error(`Error accessing microphone: ${error}`);
-    }
-  }
-  
+  // function getAudioData(numSamples): Int16Array {
+  //   // get audio frame of size `numSamples`
+  // }
+
   // let percentage = 0;
   // while (percentage < 100) {
   //   const audioData = getAudioData(eagleProfiler.minEnrollSamples);
