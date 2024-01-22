a10ca5d80d7934a275657e3f1b283bb757b76009
deden3791 <aeden3791@gmail.com>
2024-01-22 10:54:44 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# L4Project
-This is a repository for my level 4 dissertation project. 
--- main/my-app/src/pages/profile.tsx
+++
@@ -1,7 +0,0 @@
-import { UserProfile } from "@clerk/clerk-react";
- 
-const UserProfilePage = () => (
-  <UserProfile path="/profile" routing="path" />
-);
- 
-export default UserProfilePage;
